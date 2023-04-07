9c88bce84b060205b32da657f1c912a7f740dcd5
Jovan Danilovic <joco-95@hotmail.com>
2023-04-07 11:49:26 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div
-        style={{
-          position: "fixed",
-          background: "hsl(195deg, 20%, 86%)",
-          height: "40%",
-          bottom: "0",
-          width: "100%",
-        }}
-      ></div>
       <CharacterEditor />
       <Footer />
     </>
